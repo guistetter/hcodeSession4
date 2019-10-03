790f47e149b37e29cfe0b7b1b019218ca36c3afe
stetter <guilherme_stetter@hotmail.com>
2019-10-03 12:58:45 -0300

    servidor configurado com express

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const express = require('express');//carregamos modulo http
+let app = express(); 
 
-let server = http.createServer((req, res)=>{
-asdasdasdasdas
-    console.log('url:', req.url);
-    console.log('method:', req.method);
-    switch(req.url){ //rotas urls..
-        case'/':
-        res.statusCode = 200;
-        res.setHeader('Content.type', 'text/html');//setamos o cabecalho para ter certeza que é um html assim não escrevemos a tag na tela.. ele interpreta
-        res.end('<h1>Olá</h1>');
-        break;
-        case '/users':
-        res.statusCode = 200;
-        res.setHeader('Content.type', 'application/json');
-        res.end(JSON.stringify({//convertendo js para json
-            users:[{
-                name: 'hcode',
-                email:'contato',
-                id: 1
-            }]
-        }));
-    }
-
+app .get('/', (req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content.type', 'text/html');//setamos o cabecalho para ter certeza que é um html assim não escrevemos a tag na tela.. ele interpreta
+    res.end('<h1>Olá</h1>');
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+app.get('/users', (req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content.type', 'application/json');
+    res.json ({// não é necessario JSON.stringify
+        users:[{
+            name: 'hcode',
+            email:'contato',
+            id: 1
+        }]
+    });
+})
+
+app.listen(3000, '127.0.0.1', ()=>{
     console.log('servidor rodando');
 })
