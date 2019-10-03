00b4707f43624356cc54e61cd4fe854206f9ebc4
stetter <guilherme_stetter@hotmail.com>
2019-10-03 16:02:41 -0300

    bodyParser-

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');//carregamos modulo http
 const consign = require('consign');//substitui require, use..
+const bodyParser = require('body-parser');
 let app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 consign().include('routes').into(app);
 
--- routes/users.js
+++ routes/users.js
@@ -13,15 +13,7 @@ module.exports = (app)=> {//consign é como se tivesse uma func que recebe o app
         }]
     });
 });
-app.get('/users/admin', (req,res)=>{//podemos tirar /users e deixar só /admin
-    res.statusCode = 200;
-    res.setHeader('Content.type', 'application/json');
-    res.json ({// não é necessario JSON.stringify
-        users:[{
-            name: 'outra',
-            email:'coisa',
-            id: 333
-        }]
-    });
+app.post('/users', (req,res)=>{//podemos tirar /users e deixar só /admin
+   res.json(req.body);
 });
 };
